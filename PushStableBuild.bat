9999151a1adc3fa42cdff03924da3d81bf0b5c66
Keypressingmonkey <malte-stoever@web.de>
2024-01-03 17:37:58 +0100

    Created butler / itch.io script to upload the newest build to itch.io

---
+++ PushStableBuild.bat
@@ -0,0 +1,2 @@
+C:\Users\Computer\AppData\Roaming\itch\apps\butler\butler.exe push "E:\GIT\Ellen_Vague-dists\Ellen_Vague-pc.zip" codingToFreedom/ellenvague:windows
+set /p asdf="Hit enter to continue"
