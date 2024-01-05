4596a44fd0046ae8d9a3ff6e87f849496941b884
Keypressingmonkey <malte-stoever@web.de>
2024-01-05 10:27:18 +0100

    optimized images to webp

--- game/images/remove_unused_images.ps1
+++ game/images/remove_unused_images.ps1
@@ -1,12 +1,21 @@
+if (Test-Path ".\bubba.png") {Remove-Item ".\bubba.png"}
+if (Test-Path ".\bubba.jpg") {Remove-Item ".\bubba.jpg"}
+if (Test-Path ".\bubba.webp") {Remove-Item ".\bubba.jpg"}
+if (Test-Path ".\ellen_coat.png") {Remove-Item ".\ellen_coat.png"}
+if (Test-Path ".\ellen_coat.jpg") {Remove-Item ".\ellen_coat.jpg"}
+if (Test-Path ".\ellen_coat.webp") {Remove-Item ".\ellen_coat.jpg"}
+if (Test-Path ".\ellen_sweater.png") {Remove-Item ".\ellen_sweater.png"}
+if (Test-Path ".\ellen_sweater.jpg") {Remove-Item ".\ellen_sweater.jpg"}
+if (Test-Path ".\ellen_sweater.webp") {Remove-Item ".\ellen_sweater.jpg"}
 if (Test-Path ".\game_menu.png") {Remove-Item ".\game_menu.png"}
 if (Test-Path ".\game_menu.jpg") {Remove-Item ".\game_menu.jpg"}
 if (Test-Path ".\game_menu.webp") {Remove-Item ".\game_menu.jpg"}
-if (Test-Path ".\game_menu.png") {Remove-Item ".\game_menu.png"}
-if (Test-Path ".\game_menu.jpg") {Remove-Item ".\game_menu.jpg"}
-if (Test-Path ".\game_menu.webp") {Remove-Item ".\game_menu.jpg"}
-if (Test-Path ".\game_menu.png") {Remove-Item ".\game_menu.png"}
-if (Test-Path ".\game_menu.jpg") {Remove-Item ".\game_menu.jpg"}
-if (Test-Path ".\game_menu.webp") {Remove-Item ".\game_menu.jpg"}
+if (Test-Path ".\Jay Killhide.png") {Remove-Item ".\Jay Killhide.png"}
+if (Test-Path ".\Jay Killhide.jpg") {Remove-Item ".\Jay Killhide.jpg"}
+if (Test-Path ".\Jay Killhide.webp") {Remove-Item ".\Jay Killhide.jpg"}
 if (Test-Path ".\remove_unused_images.png") {Remove-Item ".\remove_unused_images.png"}
 if (Test-Path ".\remove_unused_images.jpg") {Remove-Item ".\remove_unused_images.jpg"}
 if (Test-Path ".\remove_unused_images.webp") {Remove-Item ".\remove_unused_images.jpg"}
+if (Test-Path ".\sarah.png") {Remove-Item ".\sarah.png"}
+if (Test-Path ".\sarah.jpg") {Remove-Item ".\sarah.jpg"}
+if (Test-Path ".\sarah.webp") {Remove-Item ".\sarah.jpg"}
