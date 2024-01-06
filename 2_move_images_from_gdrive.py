74ef33c6606d671c6f94e480e4c8461a779aaec2
Keypressingmonkey <malte-stoever@web.de>
2024-01-06 16:43:37 +0100

    added script to move rendered images out of Google Drive folder if they exist (my new render location so I don't have to always check my main pc)

---
+++ 2_move_images_from_gdrive.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+path_to_images_folder = "G:\My Drive\EllenVague\images"
+
+for file in os.listdir(path_to_images_folder):
+    if file.endswith(".png"):
+        shutil.move(os.path.join(path_to_images_folder, file),os.path.join(os.getcwd(), "game","images",file))
