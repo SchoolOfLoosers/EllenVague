d96774e65c316f7791f16bea0c12bdf8e6d3dc72
Keypressingmonkey <malte-stoever@web.de>
2024-01-07 14:27:40 +0100

    expended voice script to clean up old files in case lines of text are changed / new IDs generated.

--- 3_clean_up_voice_export.py
+++ 3_clean_up_voice_export.py
@@ -7,13 +7,16 @@ def main():
         set_api_key(f.readline().strip())
     all_input_lines = []
     all_dialog_lines = []
+    all_dialog_ids = []
     with open(os.path.join(os.getcwd(), "dialogue.tab"), "r") as f:
         all_input_lines = f.readlines()
         all_input_lines.pop(0)
 
+
     for line in all_input_lines:
         dialog_line = line.split("\t")
         all_dialog_lines.append(dialog_line)
+        all_dialog_ids.append(dialog_line[0])
         savepath = os.path.join(os.getcwd(), "game","audio", "voice",dialog_line[0]+".mp3")
         if (not os.path.exists(savepath) and not dialog_line[2]=="Not implemented yet"):
             print("Generating file ID: "+ dialog_line[0] + ", Speaker: "+dialog_line[1]+ ", text: " + dialog_line[2])
@@ -48,6 +51,10 @@ def main():
             )
             audio.hex
             save(audio, savepath)
+    #remove old voice files that are superseded by new IDs
+    for soundfile in os.listdir(os.path.join(os.getcwd(), "game","audio","voice")):
+        if soundfile.split('.')[0] not in all_dialog_ids:
+            os.remove(os.path.join(os.getcwd(), "game","audio","voice",soundfile))
 
 if __name__ == '__main__':
     main()
