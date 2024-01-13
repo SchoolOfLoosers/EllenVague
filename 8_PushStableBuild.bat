c6d67f5dd7cc8484b16ecfe3191dd79e480b26dd
Keypressingmonkey <malte-stoever@web.de>
2024-01-13 16:39:37 +0100

    build configs updated for new build targets

--- 8_PushStableBuild.bat
+++ 8_PushStableBuild.bat
@@ -1,3 +1,5 @@
 @echo off
 C:\Users\Computer\AppData\Roaming\itch\apps\butler\butler.exe push "E:\GIT\Ellen_Vague-dists\Ellen_Vague-pc.zip" codingToFreedom/ellenvague:windows
+C:\Users\Computer\AppData\Roaming\itch\apps\butler\butler.exe push "E:\GIT\Ellen_Vague-dists\Ellen_Vague-mac.zip" codingToFreedom/ellenvague:mac
+C:\Users\Computer\AppData\Roaming\itch\apps\butler\butler.exe push "E:\GIT\Ellen_Vague-dists\ellen.vague-release.apk" codingToFreedom/ellenvague:android
 set /p asdf="Hit enter to continue"
